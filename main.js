f9d011b519be55d322d7721df31bd166daf94c87
Adhyan Jain <71976554+adhyan-jain@users.noreply.github.com>
2025-10-15 15:44:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-
+const express = require("express");
 
 let OWNER_NUMBER = process.env.OWNER_NUMBER;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("WhatsApp Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    headless: false,
+    headless: true,
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
       "--disable-extensions",
+      "--disable-gpu",
       "--disable-software-rasterizer",
     ],
   },
@@ -30,6 +42,7 @@ client.on("ready", () => {
 
 client.on("message_create", async (msg) => {
   console.log("From:", msg.from, "Body:", msg.body, "fromMe:", msg.fromMe);
+
   if (msg.body === "!everyone") {
     try {
       const chat = await msg.getChat();
